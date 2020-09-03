c4b5bc50c3b6378691534fe68ec438904288cd92
Daniel Darritchon <dedarritchon@uc.cl>
2020-09-03 15:40:35 -0400

    Updates

---
+++ precache-manifest.4956f988ca89c462d962e62b433e6bc7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4527503aecf646c2ebdaf7512f392e4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1d286c7b4ffa3d8b268c",
+    "url": "/static/css/2.ad109a12.chunk.css"
+  },
+  {
+    "revision": "0e531f42ed83f0ad4c10",
+    "url": "/static/css/main.d0c4f3fb.chunk.css"
+  },
+  {
+    "revision": "1d286c7b4ffa3d8b268c",
+    "url": "/static/js/2.6276f608.chunk.js"
+  },
+  {
+    "revision": "a73511fdd0e79fea52c5f42ad1574633",
+    "url": "/static/js/2.6276f608.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e531f42ed83f0ad4c10",
+    "url": "/static/js/main.c95b1e41.chunk.js"
+  },
+  {
+    "revision": "cb3ccd3eb684c83ffcee",
+    "url": "/static/js/runtime-main.823c8cbe.js"
+  }
+]);
