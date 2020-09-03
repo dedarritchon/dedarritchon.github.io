15b12ed47766c765fd21158b4a1354d16953dedf
Daniel Darritchon <dedarritchon@uc.cl>
2020-09-03 16:06:18 -0400

    update content

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -6,25 +6,25 @@ registerIcons();
 
 const DATA = [
     {
-        href: "https://github.com/adamalston",
+        href: "https://github.com/dedarritchon",
         aria: "Visit my GitHub profile to learn more about the open source projects I have contributed to",
         icon: ["fab", "github"],
         label: "Github",
     },
     {
-        href: "https://www.linkedin.com/in/adammalston/",
+        href: "https://www.linkedin.com/in/dedarritchon/",
         aria: "Visit my LinkedIn profile to learn more about my education and work experience",
         icon: ["fab", "linkedin"],
         label: "LinkedIn",
     },
     {
-        href: "https://drive.google.com/drive/folders/10k8NWflSYQ5laPzuWtK3bzUKzuOeas8i?usp=sharing",
+        href: "https://drive.google.com/file/d/1ASG5JTR_lZI1b3scN3kKAgulgOTe4uNh/view?usp=sharing",
         aria: "Visit Google Drive to view and download a copy of my resume",
         icon: ["fas", "file-alt"],
         label: "Resume",
     },
     {
-        href: "mailto:aalston9@gmail.com",
+        href: "mailto:dedarritchon@uc.cl",
         aria: "Open a pre-addressed email prompt to me that you can fill out",
         icon: ["fas", "paper-plane"],
         label: "Email me",
