e9cd282c592bd88a933a243c5f3e967732fed777
Daniel Darritchon <ddarritchon@adere.so>
2021-01-09 18:56:19 -0300

    Fixed Mobile Layout for spotify widget

--- src/components/Spotify.jsx
+++ src/components/Spotify.jsx
@@ -5,7 +5,7 @@ class Spotify extends Component {
         return (
             <div className="spotify-container">
                 <h3 className="spotify-text" aria-label="Now Listening on Spotify">Now Listening on Spotify</h3>
-                <img src="https://dedarritchon.vercel.app/api/spotify" alt="Listening on Spotify" />
+                <img className="spotify-image" src="https://dedarritchon.vercel.app/api/spotify" alt="Listening on Spotify" />
             </div>
         )
     }
