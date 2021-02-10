fbf2b60daea1fad6f40286ce1c9629b3afd3c234
Daniel Darritchon <ddarritchon@adere.so>
2021-02-09 23:35:11 -0300

    Added hyperlink to github widget

--- src/components/Spotify.jsx
+++ src/components/Spotify.jsx
@@ -5,7 +5,9 @@ class Spotify extends Component {
         return (
             <div className="spotify-container">
                 <h3 className="spotify-text" aria-label="Now Listening on Spotify">Now Listening on Spotify</h3>
+                <p><a href="https://open.spotify.com/user/22usjhj23c7c4s52lzjexdtmy">
                 <img className="spotify-image" src="https://dedarritchon.vercel.app/api/spotify" alt="Listening on Spotify" />
+                </a></p>
             </div>
         )
     }
